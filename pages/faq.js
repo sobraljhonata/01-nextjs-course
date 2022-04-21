87c8d6f0a01abc57947e7f2f89686c92527c7c2b
Jhonata Hudson Silva Sobral <jhonata.sobral@mobly.com.br>
2022-04-21 18:23:10 -0300

    Aulas 4.

--- pages/faq.js
+++ pages/faq.js
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
+import PageTitle from "../src/components/PageTitle";
 
 export async function getStaticProps() {
-    const FAQ_API_URL =
-      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-    const faq = await fetch(FAQ_API_URL)
-      .then((respostaDoServidor) => {
-        return respostaDoServidor.json();
-      })
-      .then((resposta) => {
-        return resposta
-      });
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
   return {
     props: {
-        faq
+      faq,
     }, // will be passed to the page component as props
   };
 }
 
 export default function FaqPage({ faq }) {
-
   return (
     <div>
+      <PageTitle>Faq - Alura Cases Campanha</PageTitle>
       <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
       <Link href="/">Ir para a home</Link>
       <ul>
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,6 +2,7 @@ import Footer from '../../components/patterns/Footer'
 import Link from '../../components/Link'
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
+import PageTitle from '../../components/PageTitle';
 
 const LOGO_ALURA_URL = '/images/alura-logo.svg';
 const SIDE_IMAGE_URL = '/images/alura-cases.png'
@@ -96,6 +97,9 @@ function SideImage() {
 export default function HomeScreen() {
   return (
     <Box>
+      <PageTitle>
+        Home - Alura Cases Campanha
+      </PageTitle>
       <Box
         as="main"
         styleSheet={{
